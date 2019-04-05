a10c9186edb31635c6d46b2ca9ef92f6c4d3f4ee
ScreenplayWeb <info@screenplayweb.com>
2019-04-05 15:46:13 -0400

    Original upload

---
+++ index.js
@@ -0,0 +1,33 @@
+//SOCKETS TUTORIAL
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	//res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	
+//-CHAT MESSAGE HANDLER=========
+	socket.on('chat message', function(msg){
+		console.log('message: ' + msg.txt);
+		io.emit('chat message', msg);
+	});//END on chat message
+	
+	
+//-NEW SCORE HANDLER============
+	socket.on('new score', function(score){
+		console.log('message: ' + score);
+		io.emit('new score', score);
+	});//END on new score
+	
+
+
+});//END on connection
+
+http.listen(3000, function(){
+	console.log('listening on :3000');
+});
