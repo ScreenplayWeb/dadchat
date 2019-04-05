e12415281dd19f007aeb26f75b864860379d4c81
ScreenplayWeb <info@screenplayweb.com>
2019-04-05 16:25:01 -0400

    Update index.js with env port

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ io.on('connection', function(socket){
 
 });//END on connection
 
-http.listen(3000, function(){
+http.listen(process.env.PORT ||3000, function(){
 	console.log('listening on :3000');
 });
